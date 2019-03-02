a8d7fcbafc2ce163197b479c89274b0d06e895a5
noklam <channoklam@yahoo.com.hk>
2019-03-02 19:50:17 +0800

    Add create new item

--- my_project/main.js
+++ my_project/main.js
@@ -5,10 +5,10 @@ const path = require('path');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
-
+let addWindow;
 // Listen for app to be ready
 app.on('ready', function () {
-    // CReate new window
+    // Create new window
     mainWindow = new BrowserWindow({});
     // Load html into window
     mainWindow.loadURL(url.format({
@@ -24,13 +24,34 @@ app.on('ready', function () {
     Menu.setApplicationMenu(mainMenu) // set menu from self-defined menuTemplate
 });
 
+// Handle create add window
+function createAddWindow() {
+    // Create new window
+    addWindow = new BrowserWindow({
+        width: 200,
+        height: 300,
+        title:'Add Shopping List Item for myself'
+    });
+    // Load html into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'add_window.html'), // Current Directory
+        protocol: 'file:',
+        slashes: true
+    }
+    ));    //passing this path to loadURL  file://dirname/mainWindow.html
+
+}
 // Create menu template
 const mainMenuTemplate = [ // In Electron, menu is an array of object
     {
         label: 'File',
         submenu: [
             {
-                label: 'Add Some Item'
+                label: 'Add Some Item',
+                click(){
+                    createAddWindow();
+                }
+
             },
             {
                 label: 'Clear Items'
