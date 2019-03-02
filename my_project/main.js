17c4ff1c70addd751be473990f3e74923b652464
noklam <channoklam@yahoo.com.hk>
2019-03-02 19:23:49 +0800

    update, Create First Custom Define Menu

---
+++ my_project/main.js
@@ -0,0 +1,32 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+app.on('ready', function () {
+    // CReate new window
+    mainWindow = new BrowserWindow({});
+    // Load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'main_window.html'), // Current Directory
+        protocol: 'file:',
+        slashes: true
+    }
+    ));    //passing this path to loadURL  file://dirname/mainWindow.html
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu) // set menu from self-defined menuTemplate
+});
+
+// Create menu template
+const mainMenuTemplate = [ // In Electron, menu is an array of object
+    {
+        label:'File'
+    }
+];
